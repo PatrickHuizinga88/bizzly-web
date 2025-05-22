ddb50a151311605a88fed1a864c3f415a99f6ff9
PatrickHuizinga88 <patrickhuizinga44@gmail.com>
2025-05-22 21:37:42 +0200

    Improved SEO with meta tags, generated sitemap and robots.txt Added Umami analytics script Added AOS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import vue from '@astrojs/vue';
-
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bizzly.nl',
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [vue()],
+  integrations: [vue(), sitemap()],
   adapter: netlify()
 });
